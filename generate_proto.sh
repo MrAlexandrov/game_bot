455268c1225f86e32999c4358b24de506395f185
Maxim Alexandrov <maxim_0101@bk.ru>
2025-08-10 01:00:35 +0300

    Trying fix imports

--- generate_proto.sh
+++ generate_proto.sh
@@ -9,8 +9,9 @@ echo "Generating Python gRPC code from proto files..."
 # Generate Python gRPC code from proto files
 python3 -m grpc_tools.protoc \
   -I./proto \
-  --python_out=./proto \
-  --grpc_python_out=./proto \
+  --python_out=. \
+  --grpc_python_out=. \
+  --proto_path=./proto \
   ./proto/models/models.proto \
   ./proto/models/game.proto \
   ./proto/handlers/cruds.proto \
@@ -23,47 +24,6 @@ if [ $? -eq 0 ]; then
     # Add UTF-8 encoding declaration to all generated files
     find proto -name "*_pb2.py" -exec sed -i '1i\# -*- coding: utf-8 -*-' {} \;
     find proto -name "*_pb2_grpc.py" -exec sed -i '1i\# -*- coding: utf-8 -*-' {} \;
-    
-    echo "Fixing import paths in generated files..."
-    
-    # Fix the specific issues you mentioned, being very precise to avoid duplicates:
-    
-    # In proto/handlers/cruds_pb2.py - fix malformed imports
-    if [ -f proto/handlers/cruds_pb2.py ]; then
-        # Fix the specific pattern: from models from ..models import models_pb2 as models_dot_models__pb2 as models_dot_models__pb2
-        # First remove any existing alias to prevent duplicates
-        sed -i 's/from models from \.\.models import models_pb2 as models_dot_models__pb2 as models_dot_models__pb2/from models from ..models import models_pb2 as models_dot_models__pb2/' proto/handlers/cruds_pb2.py
-        sed -i 's/from models from \.\.models import game_pb2 as models_dot_game__pb2 as models_dot_game__pb2/from models from ..models import game_pb2 as models_dot_game__pb2/' proto/handlers/cruds_pb2.py
-        # Then fix the malformed import
-        sed -i 's/from models from \.\.models import models_pb2/from ..models import models_pb2/' proto/handlers/cruds_pb2.py
-        sed -i 's/from models from \.\.models import game_pb2/from ..models import game_pb2/' proto/handlers/cruds_pb2.py
-        # Also fix simpler malformed imports
-        sed -i 's/from models import models_pb2/from ..models import models_pb2/' proto/handlers/cruds_pb2.py
-        sed -i 's/from models import game_pb2/from ..models import game_pb2/' proto/handlers/cruds_pb2.py
-    fi
-    
-    # In proto/handlers/cruds_pb2_grpc.py - fix malformed imports
-    if [ -f proto/handlers/cruds_pb2_grpc.py ]; then
-        # Fix the specific pattern: from handlers from . import cruds_pb2 as handlers_dot_cruds__pb2 as handlers_dot_cruds__pb2
-        # First remove any existing alias to prevent duplicates
-        sed -i 's/from handlers from \. import cruds_pb2 as handlers_dot_cruds__pb2 as handlers_dot_cruds__pb2/from handlers from . import cruds_pb2 as handlers_dot_cruds__pb2/' proto/handlers/cruds_pb2_grpc.py
-        # Then fix the malformed import
-        sed -i 's/from handlers from \. import cruds_pb2/from . import cruds_pb2/' proto/handlers/cruds_pb2_grpc.py
-        # Also fix models imports
-        sed -i 's/from models from \.\.models import models_pb2/from ..models import models_pb2/' proto/handlers/cruds_pb2_grpc.py
-    fi
-    
-    # Fix similar issues in other files
-    find proto -name "*.py" -exec sed -i 's/from models from \.\.models/from ..models/g' {} \;
-    find proto -name "*.py" -exec sed -i 's/from handlers from \./from ./g' {} \;
-    
-    # Clean up any remaining duplicate "as" clauses
-    find proto -name "*.py" -exec sed -i 's/as models_dot_models__pb2 as models_dot_models__pb2/as models_dot_models__pb2/g' {} \;
-    find proto -name "*.py" -exec sed -i 's/as models_dot_game__pb2 as models_dot_game__pb2/as models_dot_game__pb2/g' {} \;
-    find proto -name "*.py" -exec sed -i 's/as handlers_dot_cruds__pb2 as handlers_dot_cruds__pb2/as handlers_dot_cruds__pb2/g' {} \;
-    find proto -name "*.py" -exec sed -i 's/as handlers_dot_hello__pb2 as handlers_dot_hello__pb2/as handlers_dot_hello__pb2/g' {} \;
-    
-    echo "Import paths fixed!"
     
     # Show what files were generated
     echo "Generated files:"
